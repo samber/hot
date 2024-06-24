a929022293d135e3a0684847f72807aacfc79198
Samuel Berthe <dev@samuel-berthe.fr>
2024-06-24 14:55:42 +0200

    fixed syscall.Gettimeofday

--- internal/time.go
+++ internal/time.go
@@ -1,18 +1,12 @@
 package internal
 
-import (
-	"time"
-)
+import "syscall"
 
 // NowMicro returns the current time in microseconds, with microsecond precision.
 // It is twice faster than time.Now().
-// func NowMicro() int64 {
-// 	var tv syscall.Timeval
-// 	// @TODO: check error ?
-// 	syscall.Gettimeofday(&tv) //nolint:errcheck
-// 	return int64(tv.Sec)*1e3 + int64(tv.Usec)
-// }
-
 func NowMicro() int64 {
-	return time.Now().UnixNano() / 1e3
+	var tv syscall.Timeval
+	// @TODO: check error ?
+	syscall.Gettimeofday(&tv) //nolint:errcheck
+	return int64(tv.Sec)*1e6 + int64(tv.Usec)
 }
