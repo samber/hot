ec9b95e3457eef88db5273b2e241f3355edb0edb
Samuel Berthe <dev@samuel-berthe.fr>
2025-06-30 15:18:46 +0200

    :lipstick:

--- internal/time.go
+++ internal/time.go
@@ -5,7 +5,7 @@ import _ "unsafe"
 //go:linkname nanotime runtime.nanotime
 func nanotime() int64
 
-// NowNano returns the current time in nanosecond.
+// NowNano returns the current time in nanoseconds.
 // It is approximately 3 times faster than time.Now() for high-frequency operations.
 // This function uses runtime.nanotime() for better performance.
 func NowNano() int64 {
