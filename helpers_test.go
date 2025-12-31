5fc3e639503d76c99a761c399ce16e08097a5047
Samuel Berthe <dev@samuel-berthe.fr>
2025-12-31 15:26:58 +0100

    test: better formating of test timeout

--- helpers_test.go
+++ helpers_test.go
@@ -1,7 +1,9 @@
 package hot
 
 import (
-	"os"
+	"fmt"
+	"runtime"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -11,14 +13,34 @@ func testWithTimeout(t *testing.T, timeout time.Duration) { //nolint:unused
 	t.Helper()
 
 	testFinished := make(chan struct{})
-	t.Cleanup(func() { close(testFinished) })
+
+	t.Cleanup(func() {
+		close(testFinished)
+	})
+
+	line := ""
+	funcName := ""
+
+	var pc [1]uintptr
+	n := runtime.Callers(2, pc[:])
+	if n > 0 {
+		frames := runtime.CallersFrames(pc[:])
+		frame, _ := frames.Next()
+		line = frame.File + ":" + strconv.Itoa(frame.Line)
+		funcName = frame.Function
+	}
 
 	go func() {
 		select {
 		case <-testFinished:
 		case <-time.After(timeout):
-			t.Errorf("test timed out after %s", timeout)
-			os.Exit(1)
+			if line == "" || funcName == "" {
+				panic(fmt.Sprintf("Test timed out after: %v", timeout))
+			}
+			panic(fmt.Sprintf("%s: Test timed out after: %v\n%s", funcName, timeout, line))
+
+			// t.Errorf("Test timed out after: %v", timeout)
+			// os.Exit(1)
 		}
 	}()
 }
